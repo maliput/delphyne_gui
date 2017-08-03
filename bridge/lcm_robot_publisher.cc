4b6bdee1976c6978a70c56bd16d9d596fcaea78e
Alexis Pojomovsky <apojomovsky@gmail.com>
2017-08-03 18:16:33 -0400

    Changed variable names to fit convention

--- bridge/lcm_robot_publisher.cc
+++ bridge/lcm_robot_publisher.cc
@@ -15,31 +15,31 @@ int main(int argc, char *argv[]) {
     return 1;
 
   // Generate geometry message
-  drake::lcmt_viewer_geometry_data geometry;
-  geometry.type = geometry.BOX;
-  geometry.num_float_data = 3;
-  geometry.float_data.resize(geometry.num_float_data);
-  geometry.float_data[0] = 3.8808;
-  geometry.float_data[1] = 0.75;
-  geometry.float_data[2] = 0.030921;
+  drake::lcmt_viewer_geometry_data geometryMsg;
+  geometryMsg.type = geometryMsg.BOX;
+  geometryMsg.num_float_data = 3;
+  geometryMsg.float_data.resize(geometryMsg.num_float_data);
+  geometryMsg.float_data[0] = 3.8808;
+  geometryMsg.float_data[1] = 0.75;
+  geometryMsg.float_data[2] = 0.030921;
 
   // Generate link message
-  drake::lcmt_viewer_link_data link;
-  link.name = "chassis";
-  link.robot_num = 0;
-  link.num_geom = 1;
-  link.geom.resize(link.num_geom);
-  link.geom[0] = geometry;
+  drake::lcmt_viewer_link_data linkMsg;
+  linkMsg.name = "chassis";
+  linkMsg.robot_num = 0;
+  linkMsg.num_geom = 1;
+  linkMsg.geom.resize(linkMsg.num_geom);
+  linkMsg.geom[0] = geometryMsg;
 
   // Generate robot message
-  drake::lcmt_viewer_load_robot robot_data;
-  robot_data.num_links = 1;
-  robot_data.link.resize(robot_data.num_links);
-  robot_data.link[0] = link;
+  drake::lcmt_viewer_load_robot robotMsg;
+  robotMsg.num_links = 1;
+  robotMsg.link.resize(robotMsg.num_links);
+  robotMsg.link[0] = linkMsg;
 
   // Publish robot messages at 1Hz.
   while (1) {
-    lcm.publish("DRAKE_VIEWER_LOAD_ROBOT", &robot_data);
+    lcm.publish("DRAKE_VIEWER_LOAD_ROBOT", &robotMsg);
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   }
 }

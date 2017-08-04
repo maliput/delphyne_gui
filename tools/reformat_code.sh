17fb2d67f4f164c17b0a7f54530c0781da49a5e6
Chris Lalancette <clalancette@osrfoundation.org>
2017-08-04 20:34:56 +0000

    Add in a script to reformat code automatically.
    Signed-off-by: Chris Lalancette <clalancette@osrfoundation.org>

---
+++ tools/reformat_code.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This is a simple script to run the clang-format against all of the code
+# in the repository.  clang-format will reformat all of the code according
+# to the format described in the top-level .clang-format file in this
+# repository.  This script must be run from the top-level of the repository
+# in order to find all of the files.  It is recommended to run this before
+# opening any pull request.
+
+find . -iname '*.cc' -o -iname '*.cpp' -o -iname '*.c' -o -iname '*.hpp' -o -iname '*.hh' -o -iname '*.h' | while read file ; do
+    echo "Reformatting $file"
+    clang-format-3.9 -i -style=file "$file"
+done

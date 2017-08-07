faf107c6cffaac82da1c80bedae05954d61d0fd6
Andres Fortier <andres@creativa77.com>
2017-08-07 19:26:24 -0300

    Do not change drake imports

--- tools/reformat_code.sh
+++ tools/reformat_code.sh
@@ -7,7 +7,7 @@
 # in order to find all of the files.  It is recommended to run this before
 # opening any pull request.
 
-find . -iname '*.cc' -o -iname '*.cpp' -o -iname '*.c' -o -iname '*.hpp' -o -iname '*.hh' -o -iname '*.h' | while read file ; do
+find . -not \( -path bridge/drake -prune \) -iname '*.cc' -o -iname '*.cpp' -o -iname '*.c' -o -iname '*.hpp' -o -iname '*.hh' -o -iname '*.h' | while read file ; do
     echo "Reformatting $file"
     clang-format-3.9 -i -style=file "$file"
 done

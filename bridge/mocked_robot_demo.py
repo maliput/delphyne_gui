d3efdbb6084e64d5cdf8fc9a5c39dc0127655b62
Andres Fortier <andres@creativa77.com>
2017-08-08 11:49:34 -0300

    Tweak names and add a simple way of launching both the bridge and the mocked demo

---
+++ bridge/mocked_robot_demo.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import time
+import subprocess
+
+"""
+Bazel doesn't allow us to run two different "bazel run ...." commands
+at the same time, so we need to have a separate program that launches
+both the bridge and the mock publisher.
+"""
+
+def launch(command, stdin):
+
+    label = os.path.basename(command)
+
+    if not os.path.exists(command):
+        print "Missing file %s; available files are:" % (command)
+        sys.stdout.flush()
+        subprocess.call(["/usr/bin/find", "-L", "."])
+        raise RuntimeError(command + " not found")
+
+    print("Launching %s" %(command))
+    sys.stdout.flush()
+
+    return subprocess.Popen(
+        [command],
+        stdin=stdin,
+        stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+def main():
+    bridge_path = "bridge/lcm-to-ign-trans-bridge"
+    mock_publisher_path = "bridge/lcm-mock-robot-publisher"
+    devnull = open('/dev/null')
+    p1 = launch(bridge_path, devnull)
+    time.sleep(0.5)
+    P2 = launch(mock_publisher_path, devnull)
+
+    p1.wait()
+    p2.wait()
+
+if __name__ == '__main__':
+    main()

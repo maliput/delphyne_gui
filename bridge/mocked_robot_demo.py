8618453ef642ec81672b546b81c866cb8c51fe0a
Chris Lalancette <clalancette@gmail.com>
2017-08-16 06:07:58 -0700

    Make the launcher look a lot more like the automotive demo one from d… (#23)
    * Make the launcher look a lot more like the automotive demo one from drake.
    
    We need more of that functionality.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * Remove dry_run since we don't really need it.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- bridge/mocked_robot_demo.py
+++ bridge/mocked_robot_demo.py
@@ -26,7 +26,9 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+import fcntl
 import os
+import select
 import subprocess
 import sys
 import time
@@ -37,33 +39,131 @@ at the same time, so we need to have a separate program that launches
 both the bridge and the mock publisher.
 """
 
-def launch(command, stdin):
+class TrackedProcess(object):
+    """A handy wrapper for a process object that remembers its label, and adds
+    the fileno() accessor for select() compatibility.
+    """
+    def __init__(self, label, process):
+        self.label = label
+        self.process = process
 
-    label = os.path.basename(command)
+    def fileno(self):
+        return self.process.stdout.fileno()
 
-    if not os.path.exists(command):
-        print "Missing file %s; available files are:" % (command)
-        sys.stdout.flush()
-        subprocess.call(["/usr/bin/find", "-L", "."])
-        raise RuntimeError(command + " not found")
 
-    print("Launching %s" %(command))
-    sys.stdout.flush()
+class Launcher(object):
+    """Launch and manage a group of processes as a group. It one exits, all
+    the rest are killed. The aggregate return code is that of whichever one
+    exited first.
 
-    return subprocess.Popen(
-        [command],
-        stdin=stdin,
-        stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    Any process output to stdout or stderr is echoed to the stdout of this
+    script, prefixed by process-specific labels.
+    """
+    def __init__(self):
+        self.children = []  # list of TrackedProcess
+        self.devnull = open('/dev/null')
+        self.returncode = None  # First one to exit wins.
+        self.name = os.path.basename(__file__)
+
+    def launch(self, command, label=None):
+        """Launch a process to be managed with the group. If no label is
+        supplied, a label is synthesized from the supplied command line.
+        """
+        if label is None:
+            label = os.path.basename(command[0])
+        if not os.path.exists(command[0]):
+            print "[%s] Missing file %s; available files are:" % (
+                self.name, command[0])
+            sys.stdout.flush()
+            subprocess.call(["/usr/bin/find", "-L", "."])
+            raise RuntimeError(command[0] + " not found")
+        process = subprocess.Popen(
+            command,
+            stdin=self.devnull,
+            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        flags = fcntl.fcntl(process.stdout, fcntl.F_GETFL)
+        fcntl.fcntl(process.stdout, fcntl.F_SETFL, flags | os.O_NONBLOCK)
+        self.children.append(TrackedProcess(label, process))
+
+        # Fail-fast on infant mortality.
+        time.sleep(0.05)
+        self._poll()
+        if self.returncode is not None:
+            print process.stdout.read(),
+            print "[%s] %s failed to launch" % (self.name, label)
+            sys.exit(self.returncode or 1)
+
+    def _poll(self):
+        for child in self.children:
+            ret = child.process.poll()
+            if ret is not None:
+                print "[%s] %s exited %d" % (self.name, child.label, ret)
+                if self.returncode is None:
+                    self.returncode = ret
+        return self.returncode
+
+    def _wait(self, duration):
+        done = False
+        start = time.time()
+        while not done:
+            rlist, _, _ = select.select(self.children, [], [], 0.1)
+            if self._poll() is not None:
+                done = True
+
+            now = time.time()
+            elapsed = now - start
+            if elapsed > duration:
+                print "[%s] %s exited via duration elapsed" % (
+                    self.name, self.name)
+                self.returncode = 0
+                done = True
+
+            for child in rlist:
+                try:
+                    lines = child.process.stdout.read().splitlines()
+                except IOError:
+                    lines = []
+
+                lines = [l for l in lines if l]
+                if not lines:
+                    continue
+
+                print '\n'.join(
+                    ["[%s] %s" % (child.label, line) for line in lines])
+                sys.stdout.flush()
+
+    def wait(self, duration):
+        """Wait for any of the managed processes to exit, for a keyboard
+        interrupt from the user, or for the specified duration to expire.
+        Print a message explaining which event occurred. Set the return
+        code as that of the first-exiting process, or 0 for keyboard
+        interrupt or timeout.
+        """
+        try:
+            self._wait(duration)
+        except KeyboardInterrupt:
+            # This is considered success; we ran until the user stopped us.
+            print "[%s] %s exited via keyboard interrupt" % (
+                self.name, self.name)
+            self.returncode = 0
+        assert self.returncode is not None
+
+    def kill(self):
+        """Kill any still-running managed processes."""
+        for child in self.children:
+            if child.process.poll() is None:
+                child.process.kill()
 
 def main(binaries):
-    devnull = open('/dev/null')
-    process_list = []
-    for binary in binaries:
-        process_list.append(launch(binary, devnull))
-        time.sleep(0.5)
+    launcher = Launcher()
+    try:
+        for binary in binaries:
+            launcher.launch([binary])
+        launcher.wait(float('Inf'))
+    finally:
+        launcher.kill()
 
-    for process in process_list:
-        process.wait()
+    sys.exit(launcher.returncode)
 
 if __name__ == '__main__':
     main(sys.argv[1:])

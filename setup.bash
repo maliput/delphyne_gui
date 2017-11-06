4a492cdde6aeaf33804d936443851eb599dc9217
Alexis Pojomovsky <apojomovsky@gmail.com>
2017-11-06 15:37:00 -0300

    Updated setup.bash

--- setup.bash
+++ setup.bash
@@ -2,6 +2,6 @@ BASEDIR=$( realpath $( pwd ) )
 
 export PKG_CONFIG_PATH=$BASEDIR/install/lib/pkgconfig:$PKG_CONFIG_PATH
 export PATH=$BASEDIR/install/bin:$PATH
-export LD_LIBRARY_PATH=$BASEDIR/install/lib:$BASEDIR/install_drake/lib:$LD_LIBRARY_PATH
-export DELPHYNE_PACKAGE_PATH=$BASEDIR/src/drake/drake/automotive/models:$BASEDIR/src/delphyne/bridge:$BASEDIR/src/delphyne:$BASEDIR/src/delphyne-gui$DELPHYNE_PACKAGE_PATH
 export DRAKE_INSTALL_PATH=$BASEDIR/install_drake
+export LD_LIBRARY_PATH=$BASEDIR/install/lib:$DRAKE_INSTALL_PATH/lib:$LD_LIBRARY_PATH
+export DELPHYNE_PACKAGE_PATH=$BASEDIR/src/drake/drake/automotive/models:$BASEDIR/src/delphyne/bridge:$BASEDIR/src/delphyne:$BASEDIR/src/delphyne-gui:$DELPHYNE_PACKAGE_PATH

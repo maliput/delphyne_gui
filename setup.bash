81fb87e9486740e8763ab35ca8a05e5181d95eff
Chris Lalancette <clalancette@openrobotics.org>
2017-11-14 14:23:15 +0000

    Just export DRAKE_INSTALL_PATH.
    In particular, we have to make sure to have it not have a colon
    on the end, otherwise we fail to find resources properly.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- setup.bash
+++ setup.bash
@@ -44,9 +44,9 @@ add_if_not_in_var() {
      fi
 }
 
+export DRAKE_INSTALL_PATH=$WS_DIR/install_drake
 add_if_not_in_var PATH $WS_DIR/install/bin
 add_if_not_in_var PKG_CONFIG_PATH $WS_DIR/install/lib/pkgconfig
-add_if_not_in_var DRAKE_INSTALL_PATH $WS_DIR/install_drake
 add_if_not_in_var LD_LIBRARY_PATH $WS_DIR/install_drake/lib
 add_if_not_in_var LD_LIBRARY_PATH $WS_DIR/install/lib
 add_if_not_in_var DELPHYNE_PACKAGE_PATH $WS_DIR/src/drake/drake/automotive/models

4e4537210f7362ab763cd90c6dc701853599e58b
Chris Lalancette <clalancette@openrobotics.org>
2017-11-14 17:58:56 +0000

    Re-indent add_if_not_in_var
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- setup.bash
+++ setup.bash
@@ -12,40 +12,40 @@ WS_DIR=$( dirname $SRC_DIR )
 # if the new item is not already in the environment variable.  The function
 # expects the elements of the environment variable to be separated via ':'.
 add_if_not_in_var() {
-     var=$1
-     needle=$2
+    var=$1
+    needle=$2
 
-     # Get the current list of items in the variable using the "two-pass"
-     # bash trick.  In the first pass (before the eval), $var (using the inner
-     # dollar) is expanded to the name of the environment variable.  Then the
-     # eval is run, and the outer dollar expands to the contents of the named
-     # variable.  Note that we have to escape the outer one so it doesn't get
-     # expanded by the first shell.
-     current=$( eval echo \$$var )
+    # Get the current list of items in the variable using the "two-pass"
+    # bash trick.  In the first pass (before the eval), $var (using the inner
+    # dollar) is expanded to the name of the environment variable.  Then the
+    # eval is run, and the outer dollar expands to the contents of the named
+    # variable.  Note that we have to escape the outer one so it doesn't get
+    # expanded by the first shell.
+    current=$( eval echo \$$var )
 
-     # Iterate over every item in the environment variable, looking to see
-     # if the needle is already in there.  Note that we do it this way instead
-     # of a substring because the substring can have some false positives (think
-     # about the case where "foobar" is already there while trying to add
-     # "foo").
-     while IFS=':' read -ra ADDR; do
-         found=0
-         for i in "${ADDR[@]}"; do
-             if [ "$i" == "$needle" ]; then
-                 found=1
-                 break
-             fi
-         done
-     done <<< "$current"
+    # Iterate over every item in the environment variable, looking to see
+    # if the needle is already in there.  Note that we do it this way instead
+    # of a substring because the substring can have some false positives (think
+    # about the case where "foobar" is already there while trying to add
+    # "foo").
+    while IFS=':' read -ra ADDR; do
+        found=0
+        for i in "${ADDR[@]}"; do
+            if [ "$i" == "$needle" ]; then
+                found=1
+                break
+            fi
+        done
+    done <<< "$current"
 
-     # If we didn't find the needle in the list, add it here.
-     if [ $found -eq 0 ]; then
-         if [ "$current" == "" ]; then
-             export $var=$needle
-         else
-             export $var=$( eval echo \$$var ):$needle
-         fi
-     fi
+    # If we didn't find the needle in the list, add it here.
+    if [ $found -eq 0 ]; then
+        if [ "$current" == "" ]; then
+            export $var=$needle
+        else
+            export $var=$( eval echo \$$var ):$needle
+        fi
+    fi
 }
 
 export DRAKE_INSTALL_PATH=$WS_DIR/install_drake

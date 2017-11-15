8430361408f101bc654b3d792ec2199f9d5c7d43
Chris Lalancette <clalancette@openrobotics.org>
2017-11-15 14:22:36 +0000

    Auto-generate protobuf headers at compile time.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- visualizer/TeleopWidget.cc
+++ visualizer/TeleopWidget.cc
@@ -30,7 +30,7 @@
 #include <cmath>
 #include <string>
 
-#include "protobuf/headers/automotive_driving_command.pb.h"
+#include "protobuf/automotive_driving_command.pb.h"
 
 #include "TeleopWidget.hh"
 

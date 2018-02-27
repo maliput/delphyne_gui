328049fcc39b0ac0e14c3b1b36a5bb499891322a
Daniel Stonier <d.stonier@gmail.com>
2018-02-27 13:33:35 -0500

    [infra] uncomment apt update

--- tools/install_prereqs.sh
+++ tools/install_prereqs.sh
@@ -20,10 +20,10 @@ trap at_exit EXIT
 
 [[ "${EUID}" -eq 0 ]] || die "${me} must run as root. Please use sudo."
 
-# sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-# apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 
-# apt update
+apt update
 
 echo ""
 echo "***********************************************************************************"

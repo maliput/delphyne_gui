ae6c256b2fa70544e91d3645b63c96e2ba555074
Daniel Stonier <d.stonier@gmail.com>
2018-04-07 09:53:39 -0400

    [infra] remove unused manpage generation

--- cmake/SearchForStuff.cmake
+++ cmake/SearchForStuff.cmake
@@ -4,11 +4,6 @@ include (CheckCXXSourceCompiles)
 include (${project_cmake_dir}/FindOS.cmake)
 
 ########################################
-# Include man pages stuff
-include (${project_cmake_dir}/Ronn2Man.cmake)
-add_manpage_target()
-
-########################################
 # Find drake in unix platforms
 # In Windows we expect a call from configure.bat script with the paths
 if (NOT WIN32)

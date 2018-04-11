b1e907d7c20825fc8b0ae520d795d8054b1c845f
caguero <caguero@osrfoundation.org>
2018-04-11 16:10:41 +0200

    Rename some Ignition topics.

--- visualizer/RenderWidget.cc
+++ visualizer/RenderWidget.cc
@@ -541,7 +541,7 @@ void RenderWidget::SetInitialModels(const ignition::msgs::Model_V& _msg) {
     LoadModel(_msg.models(i));
   }
 
-  this->node.Subscribe("/drake_viewer_draw", &RenderWidget::OnUpdateScene,
+  this->node.Subscribe("visualizer/scene_update", &RenderWidget::OnUpdateScene,
                        this);
 }
 

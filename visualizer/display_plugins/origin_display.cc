c17489dda11e5f1f57f60992975a4594afd940aa
dhood <dhood@users.noreply.github.com>
2018-08-24 12:46:08 -0700

    Explicit args to axis pose creation

--- visualizer/display_plugins/origin_display.cc
+++ visualizer/display_plugins/origin_display.cc
@@ -19,7 +19,7 @@ OriginDisplay::~OriginDisplay() {}
 /////////////////////////////////////////////////
 void OriginDisplay::Initialize(const tinyxml2::XMLElement* /*_pluginElem*/) {
   const double kAxisRadius = 0.02;
-  const double kAxisLength = 10000;
+  const double kAxisLength = 10000.0;
   const double kAxisHalfLength = kAxisLength / 2.0;
   if (auto scenePtr = this->Scene().lock()) {
     // Create the visual axes.
@@ -33,16 +33,39 @@ void OriginDisplay::Initialize(const tinyxml2::XMLElement* /*_pluginElem*/) {
       axis->SetLocalScale(kAxisRadius, kAxisRadius, kAxisLength);
       axis->AddGeometry(scenePtr->CreateCylinder());
     }
-
-    const ignition::math::Pose3d kAxisPoseX(kAxisHalfLength, 0, 0, 0, IGN_PI_2,
-                                            0);
+    // X axis
+    double xPosition = kAxisHalfLength;
+    double yPosition = 0.0;
+    double zPosition = 0.0;
+    double roll = 0.0;
+    double pitch = IGN_PI_2;
+    double yaw = 0.0;
+    const ignition::math::Pose3d kAxisPoseX(xPosition, yPosition, zPosition,
+                                            roll, pitch, yaw);
     axes[0]->SetLocalPose(kAxisPoseX);
     axes[0]->SetMaterial("Default/TransRed");
-    const ignition::math::Pose3d kAxisPoseY(0, kAxisHalfLength, 0, IGN_PI_2, 0,
-                                            0);
+
+    // Y axis
+    xPosition = 0.0;
+    yPosition = kAxisHalfLength;
+    zPosition = 0.0;
+    roll = IGN_PI_2;
+    pitch = 0.0;
+    yaw = 0.0;
+    const ignition::math::Pose3d kAxisPoseY(xPosition, yPosition, zPosition,
+                                            roll, pitch, yaw);
     axes[1]->SetLocalPose(kAxisPoseY);
     axes[1]->SetMaterial("Default/TransGreen");
-    const ignition::math::Pose3d kAxisPoseZ(0, 0, kAxisHalfLength, 0, 0, 0);
+
+    // Z axis
+    xPosition = 0.0;
+    yPosition = 0.0;
+    zPosition = kAxisHalfLength;
+    roll = 0.0;
+    pitch = 0.0;
+    yaw = 0.0;
+    const ignition::math::Pose3d kAxisPoseZ(xPosition, yPosition, zPosition,
+                                            roll, pitch, yaw);
     axes[2]->SetLocalPose(kAxisPoseZ);
     axes[2]->SetMaterial("Default/TransBlue");
 

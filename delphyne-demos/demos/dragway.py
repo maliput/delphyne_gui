2549141f58bcff8e4e71072641ff1392fc8d9630
Michel Hidalgo <michel@ekumenlabs.com>
2019-07-19 18:15:32 -0300

    Refactor delphyne-dragway to use new behaviour tree-based API.
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- delphyne-demos/demos/dragway.py
+++ delphyne-demos/demos/dragway.py
@@ -9,9 +9,8 @@ The dragway demo.
 # Imports
 ##############################################################################
 
-import delphyne.roads as delphyne_roads
-import delphyne.simulation as simulation
-import delphyne.utilities as utilities
+import delphyne.trees
+import delphyne.behaviours
 
 from delphyne_gui.utilities import launch_interactive_simulation
 
@@ -30,6 +29,53 @@ def parse_arguments():
         default_duration=15.0)
     return parser.parse_args()
 
+
+def create_dragway_scenario_subtree():
+    scenario_subtree = delphyne.behaviours.maliput.Dragway(
+        name="dragway",
+        num_lanes=4,
+        length=100.0,
+        lane_width=3.7,
+        shoulder_width=3.0,
+        maximum_height=5.0
+    )
+    scenario_subtree.add_children([
+        delphyne.behaviours.agents.SimpleCar(
+            name='simple-car',
+            initial_x=0.0,
+            initial_y=1.5 * 3.7
+        ),
+        delphyne.behaviours.agents.MobilCar(
+            name='mobil-car',
+            initial_x=0.0,
+            initial_y=-0.5 * 3.7,
+            initial_heading=0.0,
+            speed=1.0
+        ),
+        delphyne.behaviours.agents.RailCar(
+            name='rail-car',
+            lane_id='Dragway_Lane_1',
+            longitudinal_position=0.0,
+            lateral_offset=0.0,
+            speed=3.0
+        ),
+        delphyne.behaviours.agents.TrajectoryAgent(
+            name='trajectory-car',
+            times=[0.0, 5.0, 10.0, 15.0, 20.0],
+            headings=[0.0, 0.0, 0.0, 0.0, 0.0],
+            waypoints=[
+                [0.0,   -5.55, 0.0],  # pylint: disable=C0326
+                [10.0,  -5.55, 0.0],  # pylint: disable=C0326
+                [30.0,  -5.55, 0.0],  # pylint: disable=C0326
+                [60.0,  -5.55, 0.0],  # pylint: disable=C0326
+                [100.0, -5.55, 0.0]
+            ]
+        )
+    ])
+
+    return scenario_subtree
+
+
 ##############################################################################
 # Main
 ##############################################################################
@@ -39,82 +85,26 @@ def main():
     """Keeping pylint entertained."""
     args = parse_arguments()
 
-    builder = simulation.AgentSimulationBuilder()
-
-    dragway = builder.set_road_geometry(
-        delphyne_roads.create_dragway(
-            name="Automotive Demo Dragway",
-            num_lanes=4,
-            length=100.0,
-            lane_width=3.7,
-            shoulder_width=3.0,
-            maximum_height=5.0
-        )
+    simulation_tree = delphyne.trees.BehaviourTree(
+        root=create_dragway_scenario_subtree()
     )
-
-    # Adds the different cars.
-    simple_car_position_x = 0.0
-    simple_car_position_y = 1.5 * 3.7
-    car_id = 0
-    utilities.add_simple_car(
-        builder,
-        name=str(car_id),
-        position_x=simple_car_position_x,
-        position_y=simple_car_position_y
-    )
-
-    car_id += 1
-    utilities.add_mobil_car(
-        builder,
-        name=str(car_id),
-        scene_x=0.0,
-        scene_y=-0.5*3.7,
-        heading=0.0,
-        speed=1.0,
-    )
-
-    car_id += 1
-    utilities.add_rail_car(
-        builder,
-        name=str(car_id),
-        lane=dragway.junction(0).segment(0).lane(1),
-        position=0.0,
-        offset=0.0,
-        speed=3.0
-    )
-
-    car_id += 1
-    times = [0.0, 5.0, 10.0, 15.0, 20.0]
-    headings = [0.0, 0.0, 0.0, 0.0, 0.0]
-    waypoints = [
-        [0.0,   -5.55, 0.0],  # pylint: disable=C0326
-        [10.0,  -5.55, 0.0],  # pylint: disable=C0326
-        [30.0,  -5.55, 0.0],  # pylint: disable=C0326
-        [60.0,  -5.55, 0.0],  # pylint: disable=C0326
-        [100.0, -5.55, 0.0]
-    ]
-    utilities.add_trajectory_agent(
-        builder,
-        str(car_id),
-        times,
-        headings,
-        waypoints
-    )
-
-    runner = simulation.SimulationRunner(
-        simulation=builder.build(),
-        time_step=0.001,  # (secs)
+    simulation_tree.setup(
         realtime_rate=args.realtime_rate,
-        paused=args.paused,
-        log=args.log,
-        logfile_name=args.logfile_name)
+        start_paused=args.paused,
+        logfile_name=args.logfile_name,
+    )
 
-    with launch_interactive_simulation(runner, bare=args.bare) as launcher:
+    time_step = 0.01
+    with launch_interactive_simulation(
+        simulation_tree.runner, bare=args.bare
+    ) as launcher:
         if args.duration < 0:
             # run indefinitely
-            runner.start()
+            print("Running simulation indefinitely.")
+            simulation_tree.tick_tock(period=time_step)
         else:
             # run for a finite time
-            print("Running simulation for {0} seconds.".format(
-                args.duration))
-            runner.run_async_for(args.duration, launcher.terminate)
+            print("Running simulation for {0} seconds.".format(args.duration))
+            simulation_tree.tick_tock(
+                period=time_step, number_of_iterations=args.duration/time_step
+            )

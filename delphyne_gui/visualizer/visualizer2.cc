d025cf0e43f4cf4a3b5e977bcd8c3e0a2303d2bd
Steve Peters <scpeters@openrobotics.org>
2021-01-12 01:21:54 -0800

    Add config for new visualizer, modify visualizer2
    This ports some functionality from visualizer0 to
    the current visualizer. So far, only the Play/Pause,
    step buttons and real-time-factor display are included.

--- delphyne_gui/visualizer/visualizer2.cc
+++ delphyne_gui/visualizer/visualizer2.cc
@@ -1,21 +1,10 @@
-/*
- * Copyright (C) 2020 Open Source Robotics Foundation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+// Copyright 2021 Toyota Research Institute
 
 #include <iostream>
+#include <string>
+
+#include <ignition/common/Console.hh>
+#include <ignition/common/Filesystem.hh>
 
 #ifndef Q_MOC_RUN
 #include <ignition/gui/Application.hh>
@@ -23,18 +12,39 @@
 #include <ignition/gui/qt.h>
 #endif
 
-//////////////////////////////////////////////////
-int main(int _argc, char** _argv) {
-  std::cout << "Hello, GUI!" << std::endl;
+#include "delphyne_gui/config.hh"
 
-  // Increase verboosity so we see all messages
-  ignition::common::Console::SetVerbosity(4);
+#include "global_attributes.hh"
+
+/// Constants.
+static const char versionStr[] = "Visualizer 0.2.0";
+static const std::string initialConfigFile =
+    ignition::common::joinPaths(DELPHYNE_INITIAL_CONFIG_PATH, "layout2_with_teleop.config");
+
+/////////////////////////////////////////////////
+int main(int _argc, char** _argv) {
+  ignition::common::Console::SetVerbosity(3);
+  ignmsg << versionStr << std::endl;
+
+  if (_argc > 1) {
+    delphyne::gui::GlobalAttributes::ParseArguments(_argc - 1, &(_argv[1]));
+  }
+
+  // If we run the visualizer as a child process (like a demo written in
+  // python), we need to ensure that it's not using a block buffer
+  // to display everything that goes to the stdout in realtime.
+  if (delphyne::gui::GlobalAttributes::HasArgument("use-line-buffer")) {
+    std::string use_line_buffer_arg = delphyne::gui::GlobalAttributes::GetArgument("use-line-buffer");
+    if (use_line_buffer_arg == "yes") {
+      setlinebuf(stdout);
+    }
+  }
 
   // Create app
   ignition::gui::Application app(_argc, _argv);
 
   // Load plugins / config
-  if (!app.LoadPlugin("Publisher")) {
+  if (!app.LoadConfig(initialConfigFile)) {
     return 1;
   }
 
